994a75ab7a115619250b6ae1b67eb70e0411c544
katiaOlem <1719110962@utectulancingo.edu.mx>
2021-03-11 06:56:12 +0000

    COMPLETADO

--- picaduras/app.py
+++ picaduras/app.py
@@ -1,5 +1,4 @@
-import web, shutil
-from web import form
+import web
 import tensorflow.keras
 from PIL import Image, ImageOps
 import numpy as np
@@ -8,16 +7,38 @@ import numpy as np
 
 urls = ('/upload', 'Upload')
 
-class Upload():
+class Upload ():
     def GET(self):
         web.header("Content-Type","text/html; charset=utf-8")
-        return """<html><head></head><body>
-<form method="POST" enctype="multipart/form-data" action="">
-<input type="file" name="myfile" />
-<br/>
-<input type="submit" />
-</form>
-</body></html>"""
+        return """<html>
+        <html><head><meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>What stung me?</title>
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+     crossorigin="anonymous"></head><body><div class="container bg-light ">
+     <center>
+		<div class="row justify-content-center mt-4 pt-4">
+			<div class="col-md-10 ">
+            <a href="https://ibb.co/27Dn5xh"><img src="https://i.ibb.co/27Dn5xh/logo.png" alt="logo" border="0" ></a>
+            <br>
+            <br>
+            <br>
+             <label class="h1">What stung me?</label> <br>
+             <br><br>
+                 <form method="POST" enctype="multipart/form-data" action="">
+                 <input type="file" name="myfile" />
+                <br/><br/>
+                <button  class="btn btn-secondary input type="submit"> Subir </button> <br> <br> 
+                  </form>
+        </center>
+        </body>
+        </div></<div>
+        </html>
+
+
+
+</html>"""
 
     def POST(self):
         x = web.input(myfile={})
@@ -28,7 +49,7 @@ class Upload():
             fout = open(filedir +'/'+ filename,'wb') # creates the file where the uploaded file should be stored
             fout.write(x.myfile.file.read()) # writes the uploaded file to the newly created file.
             fout.close() # closes the file, upload complete.
-        raise web.seeother('/upload')
+     
         np.set_printoptions(suppress=True)
         # Disable scientific notation for clarity
 
@@ -61,34 +82,31 @@ class Upload():
 
 # run the inference
         prediction = model.predict(data)
-        inicio = "<html lang='es'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'><title>Qué me pico? </title><!-- CSS only -->	<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1' crossorigin='anonymous'><link rel='icon' href='logo.png' type='image/png' /></head><body>    <div class='container bg-light '>		<div class='row justify-content-center mt-4 pt-4'>			<div class='col-md-8 '>"
-        final = "</div>        </div>    </div></body></html>"
 
         for i in prediction:
             if i[0] > 0.7:
-                print("Picadura de araña")
-                picadura= inicio+"<h1 class= font-italic class='text-danger text-center'>Te ha picado una abeja .</h1> <br> <br> <h2 class='text-info'>Remedio Casero:</h2> <h3>Para las reacciones leves a moderadas, el tratamiento consiste en quitar el aguijón, lavar la zona con agua y jabón, y aplicar compresas de hielo. Las cremas en la zona afectada pueden reducir el malestar. Las reacciones más graves pueden necesitar epinefrina.</h3><br><br><img src='https://isanidad.com/wp-content/uploads/importado/6648.jpg' align='center' width='300' height='275'> <br><br>"+final
+                data= "Picadura de Araña\n Limpie el área con agua y jabón.Ayude a la persona a permanecer calmada para reducir la propagación del veneno; no aplique ningún torniquete. Aplique una compresa fría o una bolsa de hielo envuelta en un paño."
+                return data
             elif i[1] > 0.7:
-                print("picadura de Abeja")
-                picadura= inicio+"<h1 class= font-italic class='text-danger text-center'>Te ha picado una abeja .</h1> <br> <br> <h2 class='text-info'>Remedio Casero:</h2> <h3>Para las reacciones leves a moderadas, el tratamiento consiste en quitar el aguijón, lavar la zona con agua y jabón, y aplicar compresas de hielo. Las cremas en la zona afectada pueden reducir el malestar. Las reacciones más graves pueden necesitar epinefrina.</h3><br><br><img src='https://isanidad.com/wp-content/uploads/importado/6648.jpg' align='center' width='300' height='275'> <br><br>"+final
+                data= "Picadura de Abeja \n Lavar la zona afectada con agua y jabón.Enfriar la picadura con hielo.Aplicar un antiséptico.Nunca se debe apretar la picadura de abeja o avispa para tratar de sacar el veneno,  ya que este puede extenderse \nSe puede paliar el dolor y las molestias con una crema para el picor y un antihistamínico"
+                return data
             elif i[2] > 0.7:
-                print("Picadura de Hormiga")
-                picadura= inicio+"<h1 class= font-italic class='text-danger text-center'>Te ha picado una abeja .</h1> <br> <br> <h2 class='text-info'>Remedio Casero:</h2> <h3>Para las reacciones leves a moderadas, el tratamiento consiste en quitar el aguijón, lavar la zona con agua y jabón, y aplicar compresas de hielo. Las cremas en la zona afectada pueden reducir el malestar. Las reacciones más graves pueden necesitar epinefrina.</h3><br><br><img src='https://isanidad.com/wp-content/uploads/importado/6648.jpg' align='center' width='300' height='275'> <br><br>"+final
+                data="Picadura de Hormiga \n Si alguna vez crees que te ha picado una hormiga. El veneno de las picaduras de hormigas coloradas puede producir una ligera hinchazón en la zona de la picadura, y puede que el médico quiera echarle un vistazo para asegurarse de que no tienes una reacción alérgica."
+                return data
             elif i[3] > 0.7:
-                print("Picadura de Garrapata")
-                picadura= inicio+"<h1 class= font-italic class='text-danger text-center'>Te ha picado una abeja .</h1> <br> <br> <h2 class='text-info'>Remedio Casero:</h2> <h3>Para las reacciones leves a moderadas, el tratamiento consiste en quitar el aguijón, lavar la zona con agua y jabón, y aplicar compresas de hielo. Las cremas en la zona afectada pueden reducir el malestar. Las reacciones más graves pueden necesitar epinefrina.</h3><br><br><img src='https://isanidad.com/wp-content/uploads/importado/6648.jpg' align='center' width='300' height='275'> <br><br>"+final
+                data="Picadura de Garrapata\n Utiliza pinzas pequeñas o de punta fina para agarrar la garrapata lo más cerca posible de la piel. Saca suavemente la garrapata con un movimiento ascendente lento y constante. No la tuerzas ni la aprietes. No agarres la garrapata con las manos desprotegidas.\n Los expertos no recomiendan usar vaselina, esmalte de uñas ni cerillas (fósforos) calientes para quitar garrapatas."
+                return data
             elif i[4] > 0.7:
-                print("Picadura de Mosquito")
-                picadura= inicio+"<h1 class= font-italic class='text-danger text-center'>Te ha picado una abeja .</h1> <br> <br> <h2 class='text-info'>Remedio Casero:</h2> <h3>Para las reacciones leves a moderadas, el tratamiento consiste en quitar el aguijón, lavar la zona con agua y jabón, y aplicar compresas de hielo. Las cremas en la zona afectada pueden reducir el malestar. Las reacciones más graves pueden necesitar epinefrina.</h3><br><br><img src='https://isanidad.com/wp-content/uploads/importado/6648.jpg' align='center' width='300' height='275'> <br><br>"+final
+                data="Picadura de Mosquito \n Aplica loción, crema o pasta.Aplicar una loción de calamina o una crema de hidrocortisona de venta libre en la picadura puede ayudar a aliviar la picazón. O bien, prueba a untar la picadura con una pasta preparada con bicarbonato de sodio y agua. Vuelve a aplicarla varias veces al día hasta que los síntomas desaparezcan. \nAplica una compresa fría. Intenta calmar la picadura aplicando una compresa fría o un paño frío y húmedo durante unos minutos.Toma un antihistamínico oral. Para las reacciones más fuertes, prueba tomando un antihistamínico de venta libre (Benadryl, Chlor-Trimeton y otros)."
+                return data
             elif i[5] > 0.7:
-                print("Picadura de Pulga")
-                picadura= inicio+"<h1 class= font-italic class='text-danger text-center'>Te ha picado una abeja .</h1> <br> <br> <h2 class='text-info'>Remedio Casero:</h2> <h3>Para las reacciones leves a moderadas, el tratamiento consiste en quitar el aguijón, lavar la zona con agua y jabón, y aplicar compresas de hielo. Las cremas en la zona afectada pueden reducir el malestar. Las reacciones más graves pueden necesitar epinefrina.</h3><br><br><img src='https://isanidad.com/wp-content/uploads/importado/6648.jpg' align='center' width='300' height='275'> <br><br>"+final
+                data="Picadura de Pulga \n Si crees que te ha picado una pulga, lava la picadura con agua y jabón. Aplica loción de calamina para aliviar la picazón, o un adulto puede conseguirte en la farmacia una crema que alivie la picazón. Trata de no rascarte demasiado porque las picaduras podrían infectarse"
+                return data
             elif i[6] > 0.7:
-                print("Picadura de Chinche")
-                picadura= inicio+"<h1 class= font-italic class='text-danger text-center'>Te ha picado una abeja .</h1> <br> <br> <h2 class='text-info'>Remedio Casero:</h2> <h3>Para las reacciones leves a moderadas, el tratamiento consiste en quitar el aguijón, lavar la zona con agua y jabón, y aplicar compresas de hielo. Las cremas en la zona afectada pueden reducir el malestar. Las reacciones más graves pueden necesitar epinefrina.</h3><br><br><img src='https://isanidad.com/wp-content/uploads/importado/6648.jpg' align='center' width='300' height='275'> <br><br>"+final
+                data= "Picadura de Chinche \n Si crees que te ha picado una chinche, lava la picadura con agua y jabón y ponte loción de calamina para aliviar el picor. Un adulto te puede conseguir una crema para aliviar la picazón en una farmacia o droguería.\n Intenta rascarte la picadura lo menos posible porque se te podría infectar."
+                return data
             else:
-                print("Imagen no reconocida")
-        
+                picadura= "No se reconoce la Picadura"
             return picadura
 
 
